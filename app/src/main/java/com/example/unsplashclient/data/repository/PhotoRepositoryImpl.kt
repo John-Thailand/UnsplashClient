978c74e1481e16ba79e27c4ac7bc4b7f326ebfaa
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-22 12:50:38 +0900

    データ層の実装

--- app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashclient/data/repository/PhotoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashclient.data.repository
 
+import com.example.unsplashclient.data.remote.PhotoDetailDto
 import com.example.unsplashclient.data.remote.SearchPhotosResultDto
 import com.example.unsplashclient.data.remote.UnsplashApi
 import com.example.unsplashclient.domain.repository.PhotoRepository
@@ -10,5 +11,9 @@ class PhotoRepositoryImpl @Inject constructor(
 ) : PhotoRepository {
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return api.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(photoId: String): PhotoDetailDto {
+        return api.getPhotoById(photoId)
     }
 }
